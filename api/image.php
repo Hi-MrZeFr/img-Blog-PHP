6cccfcdfef6129cd00b6350bab3874f09cf62e36
MrZeFr <89246221+SYSTEMWindows11@users.noreply.github.com>
2022-04-25 18:58:59 +0800

    Update image.php

--- api/image.php
+++ api/image.php
@@ -1,8 +1,8 @@
 <?php
 //获取随机数
-$num = rand(1,13);
+$num = rand(5,13);
 //拼接图片地址
-$picpath = "https://cdn.mrzefr.cn/IMG/Image/".$num.".jpg" ,".png";
+$picpath = "https://cdn.mrzefr.cn/IMG/Image/".$num.".jpg";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
