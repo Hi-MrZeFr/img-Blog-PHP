3f3215735ab705bdad0caa6c5f05a205a01a907d
MrZeFr <89246221+SYSTEMWindows11@users.noreply.github.com>
2022-04-25 18:59:54 +0800

    Update image.php

--- api/image.php
+++ api/image.php
@@ -2,7 +2,7 @@
 //获取随机数
 $num = rand(5,13);
 //拼接图片地址
-$picpath = "https://cdn.mrzefr.cn/IMG/Image/".$num.".jpg";
+$picpath = "https://cdn.mrzefr.cn/IMG/Image/".$num.".png";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
