ff22a43e6879fe1ca3ed5f6ab5bb7bab1e04dc6d
MrZeFr <89246221+Hi-MrZeFr@users.noreply.github.com>
2022-09-06 19:59:34 +0800

    Update image.php

--- api/image.php
+++ api/image.php
@@ -2,7 +2,7 @@
 //获取随机数
 $num = rand(5,15);
 //拼接图片地址
-$picpath = "https://cdn.afdelivr.top/npm/hexo-mrzefr-filebed@1.6.7/image/Image/".$num.".png";
+$picpath = "https://jsd.mrzefr.cn/npm/hexo-mrzefr-filebed@1.6.7/image/Image/".$num.".png";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
