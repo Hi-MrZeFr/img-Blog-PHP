0dfb6644ced5bd35efc2c62b6848b361911d4984
MrZeFr <mrzefr@mrzefr.cn>
2022-11-03 11:46:57 +0800

    Update image.php

--- api/image.php
+++ api/image.php
@@ -1,8 +1,10 @@
 <?php
+header('content-type:image/png;')
 //获取随机数
 $num = rand(5,15);
 //拼接图片地址
 $picpath = "https://cdn.afdelivr.top/npm/hexo-mrzefr-filebed@1.6.7/image/Image/".$num.".png";
 //重定位到图片
-die(header("Location: $picpath"));
+$content=file_get_contents($picpath)
+echo $content;
 ?>
