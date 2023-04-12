765292b7c057e1640b8ffdfc8fb17d2878368a54
MrZeFr <mrzefr@mrzefr.cn>
2023-04-12 18:36:13 +0800

    Update image.php

--- api/image.php
+++ api/image.php
@@ -3,7 +3,7 @@ header('content-type:image/png;')
 //获取随机数
 $num = rand(5,15);
 //拼接图片地址
-$picpath = "https://cdn.afdelivr.top/npm/hexo-mrzefr-filebed@1.6.7/image/Image/".$num.".png";
+$picpath = "https://jsd.mrzefr.cn/npm/hexo-mrzefr-filebed@1.6.7/image/Image/".$num.".png";
 //重定位到图片
 $content=file_get_contents($picpath)
 echo $content;
